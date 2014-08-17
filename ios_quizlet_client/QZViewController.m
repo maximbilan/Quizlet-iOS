3e9687308b6795373b17e332b917bf9d1f8a1eae
Maxim Bilan <maximb.mail@gmail.com>
2014-08-17 17:05:18 +0300

    Added AFNetworking to project

--- ios_quizlet_client/QZViewController.m
+++ ios_quizlet_client/QZViewController.m
@@ -17,13 +17,6 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
-}
-
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
