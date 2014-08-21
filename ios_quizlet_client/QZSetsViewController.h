ff56ee034de846600e254ebc9e0e39bf1e392dff
Maxim Bilan <maximb.mail@gmail.com>
2014-08-21 20:21:28 +0300

    Added sets requests view controller

---
+++ ios_quizlet_client/QZSetsViewController.h
@@ -0,0 +1,13 @@
+//
+//  QZSetsViewController.h
+//  ios_quizlet_client
+//
+//  Created by Maxim Bilan on 8/21/14.
+//  Copyright (c) 2014 Maxim Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QZSetsViewController : UIViewController
+
+@end
---
+++ ios_quizlet_client/QZSetsViewController.m
@@ -0,0 +1,51 @@
+//
+//  QZSetsViewController.m
+//  ios_quizlet_client
+//
+//  Created by Maxim Bilan on 8/21/14.
+//  Copyright (c) 2014 Maxim Bilan. All rights reserved.
+//
+
+#import "QZSetsViewController.h"
+
+@interface QZSetsViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
+
+@end
+
+@implementation QZSetsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
