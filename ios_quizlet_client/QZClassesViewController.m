70302a0bb4cd540783be3e60d6cf3efaa9487c0a
Maxim Bilan <maximb.mail@gmail.com>
2014-08-24 15:51:23 +0300

    Modified classes view controller

--- ios_quizlet_client/QZClassesViewController.m
+++ ios_quizlet_client/QZClassesViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "QZClassesViewController.h"
+#import "QZExamplesViewController.h"
+
+#import "Quizlet.h"
 
 @interface QZClassesViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextView *logTextView;
+@property (weak, nonatomic) IBOutlet UITextField *setIdTextField;
+@property (weak, nonatomic) IBOutlet UIButton *submitButton;
 
 @end
 
@@ -29,6 +36,48 @@
     // Do any additional setup after loading the view.
 }
 
+//QZExamplesAddSetToClass,
+//QZExamplesRemoveSetFromClass,
+//QZExamplesJoinClass,
+//QZExamplesLeaveClass,
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.logTextView.text = @"";
+    
+    switch (self.exampleId) {
+        case QZExamplesViewClass:
+        {
+//            [[Quizlet sharedQuizlet] addSet:dict success:^(id responseObject) {
+//                self.logTextView.text = [NSString stringWithFormat:@"%@", responseObject];
+//            } failure:^(NSError *error) {
+//                self.logTextView.text = [error description];
+//            }];
+        }
+        break;
+        
+        case QZExamplesViewClassSets:
+        {
+            
+        }
+        break;
+            
+        case QZExamplesAddClass:
+        case QZExamplesEditClass:
+        case QZExamplesDeleteClass:
+        {
+            self.setIdTextField.hidden = NO;
+            self.submitButton.hidden = NO;
+        }
+        break;
+            
+        default:
+            break;
+    }
+}
+
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -45,5 +94,22 @@
     // Pass the selected object to the new view controller.
 }
 */
+
+- (IBAction)submitButtonAction:(UIButton *)sender
+{
+    [self.setIdTextField resignFirstResponder];
+    
+    if (self.setIdTextField.text.length > 0) {
+        switch (self.exampleId) {
+            
+                
+            default:
+                break;
+        }
+        
+        self.setIdTextField.hidden = YES;
+        self.submitButton.hidden = YES;
+    }
+}
 
 @end
