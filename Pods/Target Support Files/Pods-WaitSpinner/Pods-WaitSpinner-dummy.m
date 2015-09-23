84c549aced1cacb031ceb808802f5ceee22363d4
Maxim Bilan <maximb.mail@gmail.com>
2015-09-23 20:46:28 +0300

    Updated pods

--- Pods/Target Support Files/Pods-WaitSpinner/Pods-WaitSpinner-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WaitSpinner : NSObject
-@end
-@implementation PodsDummy_Pods_WaitSpinner
-@end
---
+++ Pods/Target Support Files/WaitSpinner/WaitSpinner-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WaitSpinner : NSObject
+@end
+@implementation PodsDummy_WaitSpinner
+@end
